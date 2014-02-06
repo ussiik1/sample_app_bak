46d7cb9c82778e905a2ff56cf49b64df7b7fe8b6
Ushijima Koichi <ussiik001@gmail.com>
2014-02-06 16:43:00 +0900

    Add user error test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,8 +16,38 @@ describe "UserPages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_selector('li', text: "Email can't be blank") }
+        it { should have_selector('li', text: "Email is invalid") }
+        it { should have_selector('li', text: "Name can't be blank") }
+        #it { should have_selector('li', text: "Name is too long (maximum is 50 characters)") }
+        it { should have_selector('li', text: "Password can't be blank") }
+        it { should have_selector('li', text: "Password is too short (minimum is 6 characters)") }
+        it { should have_selector('li', text: "Password confirmation can't be blank") }
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+      end
+      describe "after submission with fill in Name with 50 over words" do
+        before do
+          fill_in "Name",         with: "1234567891123456789212345678931234567894123456789512345"
+          click_button submit
+        end
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_selector('li', text: "Email can't be blank") }
+        it { should have_selector('li', text: "Email is invalid") }
+        #it { should have_selector('li', text: "Name can't be blank") }
+        it { should have_selector('li', text: "Name is too long (maximum is 50 characters)") }
+        it { should have_selector('li', text: "Password can't be blank") }
+        it { should have_selector('li', text: "Password is too short (minimum is 6 characters)") }
+        it { should have_selector('li', text: "Password confirmation can't be blank") }
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
       end
     end
 
@@ -31,6 +61,13 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
