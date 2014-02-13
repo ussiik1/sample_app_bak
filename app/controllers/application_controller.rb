49bfa6bc06d32490e0d5576742d445bb3a69c594
Ushijima Koichi <ussiik001@gmail.com>
2014-02-13 20:10:06 +0900

    8.2 suceed sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # Sign out for anti CSRF weakness
+  def handle_unverified_request
+    sign_out
+    super
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140124034451) do
+ActiveRecord::Schema.define(:version => 20140207081529) do
 
   create_table "android_metadata", :id => false, :force => true do |t|
     t.text "locale"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(:version => 20140124034451) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
